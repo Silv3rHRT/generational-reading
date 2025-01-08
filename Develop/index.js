f028e619e10c587c553145df15f303a5622e059b
Silv3rHRT <somethingcuteandgay@gmail.com>
2025-01-08 17:34:53 -0500

    created login in index.js for requirements, questions, writeToFile, and initializing functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,58 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs'); //fs is used here so we can save to the file system and create a document
+const inquirer = require('inquirer')  //will not run without inquirer package installed
+const generateMarkdown = require('./generateMarkdown') //imports generateMarkdown from the generateMarkdown.js
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input', //what kind of data will be passed in, in this case we use input as we will be typing all our answers in
+        name: 'title', //creates a name key for this response
+        message: 'What is the title of your project?' //question to be asked
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the main purpose or problem the project solves?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is the project used?'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What type of license does the project have? (for example, MIT, Apache, GPL)'
+    },
+    {
+        type: 'input',
+        name: 'contact',
+        message: 'What is your GitHub username?'
+    },
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    // creates the new file and leaves a motivational console log
+    fs.writeFile(fileName, data, () => {
+        console.log('README file created! Good luck on the project! I believe in you!')
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((answers)) => {
+            const readmeContent - generateMarkdown(answers); //uses generateMarkdown function from generateMarkdown.js to create README
+            writeToFile('README.md', readmeContent); //writes generated content to the file
+        }
+}
 
 // Function call to initialize app
 init();
